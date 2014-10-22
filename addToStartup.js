360fa42d5cea7818668e49995f13aff9fa07ffbf
ladder1984 <ladder1984@gmail.com>
2014-10-22 19:58:37 +0800

    新增addToStartup.js文件，用于添加启动项

---
+++ addToStartup.js
@@ -0,0 +1,22 @@
+
+
+function CreateShortcut(target_path)
+{
+   wsh = new ActiveXObject('WScript.Shell');
+   link = wsh.CreateShortcut(wsh.SpecialFolders("Startup") + '\\updateHosts.lnk');
+   link.TargetPath = '"' + wsh.CurrentDirectory + '\\updateHosts.py"';
+   link.Arguments = '"' + wsh.CurrentDirectory + '\\updateHosts.py"';
+   link.WindowStyle = 7;
+   link.Description = 'updateHosts';
+   link.WorkingDirectory = wsh.CurrentDirectory;
+   link.Save();
+}
+
+function main()
+{
+   
+    CreateShortcut();
+
+}
+
+main();
--- updateHosts.py
+++ updateHosts.py
@@ -3,7 +3,7 @@
 #############################
 #name:updateHosts
 #author:https://github.com/ladder1984
-#version:0.0.1
+#version:0.0.2
 #python version:2.7.8
 ############################
 
