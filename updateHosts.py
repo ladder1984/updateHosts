9730e804554fd6656f9e5448eddbd688d3b10ac8
ladder1984 <ladder1984@gmail.com>
2014-10-23 19:19:43 +0800

    代码规范化；添加去除AdBlock功能

--- updateHosts.py
+++ updateHosts.py
@@ -4,23 +4,50 @@
 #name:updateHosts
 #author:https://github.com/ladder1984
 #python version:2.7.8
-#version:0.0.3
+#version:0.0.4
 ############################
 
 
 from urllib import urlretrieve
 import shutil
-from  datetime  import  *
+from datetime import *
+import re
 
-hostsSource="https://raw.githubusercontent.com/zxdrive/imouto.host/master/imouto.host.txt"
-hostsLocation="C:\Windows\System32\drivers\etc\hosts"
-errorLog=open('errorLog.txt','a')
+#setting
+hostsSource = "https://raw.githubusercontent.com/zxdrive/imouto.host/master/imouto.host.txt"
+hostsLocation = "C:\Windows\System32\drivers\etc\hosts"
+noAdBlock = 1
+#seting
+errorLog = open('errorLog.txt', 'a')
 
-try:
-    urlretrieve(hostsSource,"hosts")
-except IOError, e:
-    errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
-try:
-    shutil.move("hosts",hostsLocation)
-except IOError, e:
-    errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
+
+def download_hosts():
+    try:
+        urlretrieve(hostsSource, "hosts")
+    except IOError, e:
+        errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
+
+
+def del_adblock():
+    file_to_open = open('hosts', 'r')
+    hosts_content = file_to_open.read()
+    hosts_content = re.sub('#AdBlock START([\s\S]*)#AdBlock END', "#", hosts_content)
+    file_to_open.close()
+
+    file_to_open = open('hosts', 'w')
+    file_to_open.write(hosts_content)
+    file_to_open.close()
+
+
+def move_hosts():
+    try:
+        shutil.move("hosts", hostsLocation)
+    except IOError, e:
+        errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
+
+errorLog.close()
+
+download_hosts()
+if noAdBlock == 1:
+    del_adblock()
+move_hosts()
