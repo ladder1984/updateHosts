8ec04e73e991053387460fd83401da2f28f6281a
ladder1984 <ladder1984@gmail.com>
2014-10-23 13:02:04 +0800

    update

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ updateHosts
 - python版本：2.x
 
 ##功能描述
-从imouto.host下载hosts文件并更新本地hosts
+- 从imouto.host下载hosts文件并更新本地hosts
+- 可添加启动项
+- 生成错误日志errorLog.txt
 
-##计划功能
-- ~~自动添加启动项~~
-- ~~打包成exe~~
+##使用方法
+下载**updateHosts.py**和**addToStarup.js**到本地，直接运行updateHosts.py更新hosts，点击addToStarup.js讲py文件的快捷方式加入启动项
+
+##待添加功能
 - 备份hosts
-- Linux支持
+
--- updateHosts.py
+++ updateHosts.py
@@ -3,21 +3,24 @@
 #############################
 #name:updateHosts
 #author:https://github.com/ladder1984
-#version:0.0.2
 #python version:2.7.8
+#version:0.0.3
 ############################
 
 
 from urllib import urlretrieve
 import shutil
+from  datetime  import  *
 
 hostsSource="https://raw.githubusercontent.com/zxdrive/imouto.host/master/imouto.host.txt"
 hostsLocation="C:\Windows\System32\drivers\etc\hosts"
+errorLog=open('errorLog.txt','a')
+
 try:
     urlretrieve(hostsSource,"hosts")
-except:
-    pass
+except IOError, e:
+    errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
 try:
     shutil.move("hosts",hostsLocation)
-except:
-    pass
+except IOError, e:
+    errorLog.write(str(datetime.now())+'\n'+str(e)+'\n\n')
