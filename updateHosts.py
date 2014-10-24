5a439eb9f761befff49a7b6cbc5351acbdc641ff
ladder1984 <ladder1984@gmail.com>
2014-10-24 13:05:14 +0800

    增加替换前的备份功能

--- updateHosts.py
+++ updateHosts.py
@@ -4,21 +4,30 @@
 #name:updateHosts
 #author:https://github.com/ladder1984
 #python version:2.7.8
-#version:0.0.4
+#version:0.0.5
 ############################
 
 
 from urllib import urlretrieve
-import shutil
 from datetime import *
 import re
+import os
+import shutil
 
 #setting
 hostsSource = "https://raw.githubusercontent.com/zxdrive/imouto.host/master/imouto.host.txt"
+hosts_folder = "C:\Windows\System32\drivers\etc\\"
 hostsLocation = "C:\Windows\System32\drivers\etc\hosts"
 noAdBlock = 1
 #seting
+
 errorLog = open('errorLog.txt', 'a')
+
+
+def backup_hosts():
+    if not os.path.isfile(hosts_folder+'backup_hosts_original_by_updateHosts'):
+        shutil.copy(hosts_folder+'hosts', hosts_folder+'backup_hosts_original_by_updateHosts')
+    shutil.copy(hosts_folder+'hosts', hosts_folder+'backup_hosts_last_by_updateHosts')
 
 
 def download_hosts():
@@ -47,6 +56,8 @@ def move_hosts():
 
 errorLog.close()
 
+#main
+backup_hosts()
 download_hosts()
 if noAdBlock == 1:
     del_adblock()
