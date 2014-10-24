9c01ddb1c250d1d6e1154e0aa56a5965f18ecbcf
ladder1984 <ladder1984@gmail.com>
2014-10-25 03:11:34 +0800

    py to pyw

--- addToStartup.js
+++ addToStartup.js
@@ -2,8 +2,8 @@ function CreateShortcut(target_path)
 {
    wsh = new ActiveXObject('WScript.Shell');
    link = wsh.CreateShortcut(wsh.SpecialFolders("Startup") + '\\updateHosts.lnk');
-   link.TargetPath = '"' + wsh.CurrentDirectory + '\\updateHosts.py"';
-   link.Arguments = '"' + wsh.CurrentDirectory + '\\updateHosts.py"';
+   link.TargetPath = '"' + wsh.CurrentDirectory + '\\updateHosts.pyw"';
+   link.Arguments = '"' + wsh.CurrentDirectory + '\\updateHosts.pyw"';
    link.WindowStyle = 7;
    link.Description = 'updateHosts';
    link.WorkingDirectory = wsh.CurrentDirectory;
